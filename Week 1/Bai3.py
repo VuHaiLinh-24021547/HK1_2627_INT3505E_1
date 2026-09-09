5b03e683f5d6d45dd81ed3e96ee5003ce477f423
VuHaiLinh <24021547@vnu.edu.vn>
2026-09-09 16:14:25 +0700

    Bai3

---
+++ Week 1/Bai3.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from uuid import uuid4
+
+app = Flask(__name__)
+STUDENTS = []
+
+@app.route("/students", methods=["POST"])
+def create_student():
+    body = request.get_json(silent=True) or {}
+    name = body.get("name")
+    if not name:
+        return jsonify({"error":"name is required"}), 400
+
+    student = {
+        "id": str(uuid4()),
+        "name": name,
+        "gpa": body.get("gpa", 0.0)
+    }
+
+    STUDENTS.append(student)
+    return jsonify(student), 201
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
